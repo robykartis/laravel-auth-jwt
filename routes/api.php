88f97a149852a87e0de01133999164d51a3981dd
robykartis <robykarti.s@gmail.com>
2023-06-06 15:54:11 +0700

    API

--- routes/api.php
+++ routes/api.php
@@ -2,7 +2,6 @@
 
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\BookController;
-use Illuminate\Http\Request;
 use Illuminate\Support\Facades\Route;
 
 Route::post('register', [AuthController::class, 'register']);
